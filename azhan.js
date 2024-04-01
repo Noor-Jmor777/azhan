046d2095dd050d5f6755e2b01a81ab2a191690b2
Noor Sardar <noor.sardarr@gmail.com>
2024-04-01 05:23:51 +0300

    initial commit

---
+++ azhan.js
@@ -0,0 +1,641 @@
+let iraqiCities = [
+  {
+    name: "Baghdad",
+    country: "IQ"
+  },
+  {
+    name: "Mosul",
+    country: "IQ"
+  },
+  {
+    name: "Basra",
+    country: "IQ"
+  },
+  {
+    name: "Erbil",
+    country: "IQ"
+  },
+  {
+    name: "Kirkuk",
+    country: "IQ"
+  },
+  {
+    name: "Najaf",
+    country: "IQ"
+  },
+  {
+    name: "Karbala",
+    country: "IQ"
+  },
+  {
+    name: "Sulaymaniyah",
+    country: "IQ"
+  },
+  {
+    name: "Hilla",
+    country: "IQ"
+  },
+  {
+    name: "Fallujah",
+    country: "IQ"
+  },
+  {
+    name: "Ramadi",
+    country: "IQ"
+  },
+  {
+    name: "Nasiriyah",
+    country: "IQ"
+  },
+  {
+    name: "Kut",
+    country: "IQ"
+  },
+  {
+    name: "Diwaniyah",
+    country: "IQ"
+  },
+  {
+    name: "Amara",
+    country: "IQ"
+  },
+  {
+    name: "Samawah",
+    country: "IQ"
+  },
+  {
+    name: "Balad",
+    country: "IQ"
+  },
+  {
+    name: "Tikrit",
+    country: "IQ"
+  },
+  {
+    name: "Sinjar",
+    country: "IQ"
+  },
+  {
+    name: "Haditha",
+    country: "IQ"
+  },
+  {
+    name: "Ba'qubah",
+    country: "IQ"
+  },
+  {
+    name: "Khalis",
+    country: "IQ"
+  },
+  {
+    name: "Kufa",
+    country: "IQ"
+  },
+  {
+    name: "Samarra",
+    country: "IQ"
+  },
+  {
+    name: "Taji",
+    country: "IQ"
+  },
+  {
+    name: "Al-Kut",
+    country: "IQ"
+  },
+  {
+    name: "Hīt",
+    country: "IQ"
+  },
+  {
+    name: "Rawa",
+    country: "IQ"
+  },
+  {
+    name: "Al-Qa'im",
+    country: "IQ"
+  },
+  {
+    name: "Al-Qurnah",
+    country: "IQ"
+  }
+];
+let syrianCities = [
+  {
+    name: "Damascus",
+    country: "SY"
+  },
+  {
+    name: "Aleppo",
+    country: "SY"
+  },
+  {
+    name: "Homs",
+    country: "SY"
+  },
+  {
+    name: "Hama",
+    country: "SY"
+  },
+  {
+    name: "Latakia",
+    country: "SY"
+  },
+  {
+    name: "Raqqa",
+    country: "SY"
+  },
+  {
+    name: "Deir ez-Zor",
+    country: "SY"
+  },
+  {
+    name: "Daraa",
+    country: "SY"
+  },
+  {
+    name: "Idlib",
+    country: "SY"
+  },
+  {
+    name: "Al-Hasakah",
+    country: "SY"
+  },
+  {
+    name: "Qamishli",
+    country: "SY"
+  },
+  {
+    name: "Tartus",
+    country: "SY"
+  },
+  {
+    name: "As-Suwayda",
+    country: "SY"
+  },
+  {
+    name: "Manbij",
+    country: "SY"
+  },
+  {
+    name: "Al-Bab",
+    country: "SY"
+  },
+  {
+    name: "Jarabulus",
+    country: "SY"
+  },
+  {
+    name: "Azaz",
+    country: "SY"
+  },
+  {
+    name: "Al-Mayadin",
+    country: "SY"
+  },
+  {
+    name: "Salamiyah",
+    country: "SY"
+  },
+  {
+    name: "Yarmouk",
+    country: "SY"
+  },
+  {
+    name: "Al-Bukamal",
+    country: "SY"
+  },
+  {
+    name: "Maarrat al-Nu'man",
+    country: "SY"
+  }
+];
+let turkeyCities = [
+  {
+    name: "Istanbul",
+    country: "TR"
+  },
+  {
+    name: "Ankara",
+    country: "TR"
+  },
+  {
+    name: "Izmir",
+    country: "TR"
+  },
+  {
+    name: "Bursa",
+    country: "TR"
+  },
+  {
+    name: "Antalya",
+    country: "TR"
+  }
+];
+let uaeCities = [
+  {
+    name: "Dubai",
+    country: "AE"
+  },
+  {
+    name: "Abu Dhabi",
+    country: "AE"
+  },
+  {
+    name: "Sharjah",
+    country: "AE"
+  },
+  {
+    name: "Ajman",
+    country: "AE"
+  },
+  {
+    name: "Fujairah",
+    country: "AE"
+  }
+];
+let yemenCities = [
+  {
+    name: "Sana'a",
+    country: "YE"
+  },
+  {
+    name: "Aden",
+    country: "YE"
+  },
+  {
+    name: "Taiz",
+    country: "YE"
+  },
+  {
+    name: "Al Hudaydah",
+    country: "YE"
+  },
+  {
+    name: "Ibb",
+    country: "YE"
+  }
+];
+let jordanCities = [
+  {
+    name: "Amman",
+    country: "JO"
+  },
+  {
+    name: "Zarqa",
+    country: "JO"
+  },
+  {
+    name: "Irbid",
+    country: "JO"
+  },
+  {
+    name: "Aqaba",
+    country: "JO"
+  },
+  {
+    name: "Madaba",
+    country: "JO"
+  }
+];
+let kuwaitCities = [
+  {
+    name: "Kuwait City",
+    country: "KW"
+  },
+  {
+    name: "Salmiya",
+    country: "KW"
+  },
+  {
+    name: "Hawalli",
+    country: "KW"
+  },
+  {
+    name: "Farwaniya",
+    country: "KW"
+  },
+  {
+    name: "Al Ahmadi",
+    country: "KW"
+  }
+];
+let lebanonCities = [
+  {
+    name: "Beirut",
+    country: "LB"
+  },
+  {
+    name: "Tripoli",
+    country: "LB"
+  },
+  {
+    name: "Sidon",
+    country: "LB"
+  },
+  {
+    name: "Tyre",
+    country: "LB"
+  },
+  {
+    name: "Byblos",
+    country: "LB"
+  }
+];
+let omanCities = [
+  {
+    name: "Muscat",
+    country: "OM"
+  },
+  {
+    name: "Salalah",
+    country: "OM"
+  },
+  {
+    name: "Sohar",
+    country: "OM"
+  },
+  {
+    name: "Nizwa",
+    country: "OM"
+  },
+  {
+    name: "Sur",
+    country: "OM"
+  }
+];
+let palestineCities = [
+  {
+    name: "Quds",
+    country: "PS"
+  },
+  {
+    name: "Gaza City",
+    country: "PS"
+  },
+  {
+    name: "Hebron",
+    country: "PS"
+  },
+  {
+    name: "Nablus",
+    country: "PS"
+  },
+  {
+    name: "Ramallah",
+    country: "PS"
+  }
+];
+let qatarCities = [
+  {
+    name: "Doha",
+    country: "QA"
+  },
+  {
+    name: "Al Wakrah",
+    country: "QA"
+  },
+  {
+    name: "Al Khor",
+    country: "QA"
+  },
+  {
+    name: "Al Rayyan",
+    country: "QA"
+  },
+  {
+    name: "Umm Salal Mohammed",
+    country: "QA"
+  }
+];
+let saudiArabiaCities = [
+  {
+    name: "Riyadh",
+    country: "SA"
+  },
+  {
+    name: "Jeddah",
+    country: "SA"
+  },
+  {
+    name: "Mecca",
+    country: "SA"
+  },
+  {
+    name: "Medina",
+    country: "SA"
+  },
+  {
+    name: "Dammam",
+    country: "SA"
+  }
+];
+let afghanCities = [
+  {
+    name: "Kabul",
+    country: "AF"
+  },
+  {
+    name: "Kandahar",
+    country: "AF"
+  },
+  {
+    name: "Herat",
+    country: "AF"
+  },
+  {
+    name: "Mazar-i-Sharif",
+    country: "AF"
+  },
+  {
+    name: "Jalalabad",
+    country: "AF"
+  }
+];
+let bahrainCities = [
+  {
+    name: "Manama",
+    country: "BH"
+  },
+  {
+    name: "Riffa",
+    country: "BH"
+  },
+  {
+    name: "Muharraq",
+    country: "BH"
+  },
+  {
+    name: "Hamad Town",
+    country: "BH"
+  },
+  {
+    name: "Isa Town",
+    country: "BH"
+  }
+];
+let cyprusCities = [
+  {
+    name: "Nicosia",
+    country: "CY"
+  },
+  {
+    name: "Limassol",
+    country: "CY"
+  },
+  {
+    name: "Larnaca",
+    country: "CY"
+  },
+  {
+    name: "Famagusta",
+    country: "CY"
+  },
+  {
+    name: "Paphos",
+    country: "CY"
+  }
+];
+let egyptCities = [
+  {
+    name: "Cairo",
+    country: "EG"
+  },
+  {
+    name: "Alexandria",
+    country: "EG"
+  },
+  {
+    name: "Giza",
+    country: "EG"
+  },
+  {
+    name: "Shubra El-Kheima",
+    country: "EG"
+  },
+  {
+    name: "Port Said",
+    country: "EG"
+  }
+];
+let iranCities = [
+  {
+    name: "Tehran",
+    country: "IR"
+  },
+  {
+    name: "Mashhad",
+    country: "IR"
+  },
+  {
+    name: "Isfahan",
+    country: "IR"
+  },
+  {
+    name: "Karaj",
+    country: "IR"
+  },
+  {
+    name: "Shiraz",
+    country: "IR"
+  }
+];
+const countries = {
+  "Afghanistan": afghanCities,
+  "Bahrain": bahrainCities,
+  "Cyprus": cyprusCities,
+  "Egypt": egyptCities,
+  "Iran": iranCities,
+  "Iraq": iraqiCities,
+  "Jordan": jordanCities,
+  "Kuwait": kuwaitCities,
+  "Lebanon": lebanonCities,
+  "Oman": omanCities,
+  "Palestine": palestineCities,
+  "Qatar": qatarCities,
+  "Saudi Arabia": saudiArabiaCities,
+  "Syria": syrianCities,
+  "Turkey": turkeyCities,
+  "UAE": uaeCities,
+  "Yemen": yemenCities
+};
+
+const countrySelect = document.getElementById("countrySelect");
+const citySelect = document.getElementById("citySelect");
+
+//load countries in the input options
+Object.keys(countries).forEach((country) => {
+  const option = document.createElement("option");
+  option.value = country;
+  option.textContent = country;
+  countrySelect.appendChild(option);
+});
+
+// load cities in the input options
+function loadCities()
+{
+  citySelect.innerHTML ="";
+  let cities = [];
+  
+  cities = countries[countrySelect.value] || [];
+
+  cities.forEach((city) => {
+    const option = document.createElement("option");
+    option.value = city.name;
+    option.textContent = city.name;
+    citySelect.appendChild(option);
+  });
+  LoadTimes(citySelect.value,countrySelect.value); // by default
+
+  citySelect.addEventListener("change", function() {
+    //const itsCountry = cities.find(city => city.name === citySelect.value).country;
+    LoadTimes(citySelect.value,countrySelect.value);
+  });
+}
+loadCities();
+countrySelect.addEventListener("change",loadCities)
+
+function LoadTimes(city,country) {
+  let params = 
+  {
+    city : city,
+    country : country
+  };
+  axios.get('http://api.aladhan.com/v1/timingsByCity',{params:params})
+  .then(data => {
+    const showTimes = document.getElementById("showTimes");
+    const showCity =  document.getElementById("showCity")
+    showTimes.innerHTML = '';
+    showCity.innerHTML = '';
+    const dateShow = data.data.data.date.gregorian.date;
+    const dayShow = data.data.data.date.gregorian.weekday.en;
+
+    showCity.innerHTML += `<h3>${country}/${city}</h3>`;
+    showCity.innerHTML += `<h5>${dateShow}</h5>`;
+    showCity.innerHTML += `<h6>${dayShow}</h6>`;
+
+    const timings = data.data.data.timings;
+    const timingsEntries = Object.entries(timings).slice(0, 7);
+    timingsEntries.forEach(([key, value]) => {
+
+      const timeElement = document.createElement("div");
+      timeElement.classList.add("timeElement","bg-warning","bg-opacity-25","rounded-3","overflow-hidden","mb-2");
+      const keyTime = document.createElement("h4");
+      keyTime.classList.add("p-2","w-100","bg-black","text-white")
+      const ValueTime = document.createElement("h4");
+      ValueTime.classList.add("p-2","w-100","fw-bold")
+
+      keyTime.textContent = `${key}`;
+      ValueTime.textContent = `${value}`;
+      
+      timeElement.appendChild(keyTime);
+      timeElement.appendChild(ValueTime);
+      showTimes.appendChild(timeElement);
+    });
+  })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    }
+  );
+}
+
